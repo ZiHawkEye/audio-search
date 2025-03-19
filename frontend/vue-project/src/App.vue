37f3cff223b1732e36828d94ab8986e799145113
ZiHawkEye <yewkaizhe@hotmail.com>
2025-03-19 15:17:43 +0800

    Added audio upload to frontend

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,47 +1,55 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <img alt="Vue logo" src="./assets/logo.png">
+	<single-file/>
+	<multiple-files/>
+	<select-files/>
+	<file-progress/>
+	<drag-and-drop/>
+	<drag-and-drop-instant/>
+	<single-image-preview/>
+	<multiple-images-preview/>
+	<audio-preview/>
+	<video-preview/>
+	<csv-preview/>
+	<preview-csv/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import SingleFile from './components/SingleFile.vue'
+import MultipleFiles from './components/MultipleFiles.vue';
+import SelectFiles from './components/SelectFiles.vue';
+import FileProgress from './components/FileProgress.vue';
+import DragAndDrop from './components/DragAndDrop.vue';
+import DragAndDropInstant from './components/DragAndDropInstant.vue';
+import SingleImagePreview from './components/SingleImagePreview.vue';
+import MultipleImagesPreview from './components/MultipleImagesPreview.vue';
+import AudioPreview from './components/AudioPreview.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SingleFile,
+	MultipleFiles,
+	SelectFiles,
+	FileProgress,
+	DragAndDrop,
+	DragAndDropInstant,
+	SingleImagePreview,
+	MultipleImagesPreview,
+	AudioPreview,
+  }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+	max-width: 600px;
+	margin: auto;
 }
 </style>
