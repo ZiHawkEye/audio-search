c810b375958425487833e4eef9a53972c2ddfc48
ZiHawkEye <yewkaizhe@hotmail.com>
2025-03-20 16:56:40 +0800

    Remove unnecessary files

--- frontend/vue-project/src/components/AudioPreview.vue
+++ frontend/vue-project/src/components/AudioPreview.vue
@@ -1,64 +1,67 @@
 // Reference: https://github.com/canopas/vue-file-upload/tree/main
 <template>
-	<div class="container">
-		<div>
-			<label>Upload an audio file for transcription
-				<input type="file" accept="audio/*" @change="handleFileUpload( $event )"/>
-			</label>
-			<br>
-			<audio id="audio-preview" controls v-show="file != ''"/>
-			<br>
-			<button v-on:click="submitFile()">Submit File</button>
-		</div>
-	</div>
+  <div class="container">
+    <div>
+      <label
+        >Upload an audio file for transcription
+        <input
+          type="file"
+          accept="audio/*"
+          @change="handleFileUpload($event)"
+        />
+      </label>
+      <br />
+      <audio id="audio-preview" controls v-show="file != ''" />
+      <br />
+      <button v-on:click="submitFile()">Submit File</button>
+    </div>
+  </div>
 </template>
 
 <script>
-	import axios from 'axios';
-	
-	export default {
-		data(){
-			return {
-				file: ''
-			}
-		},
-		
-		methods: {
-			handleFileUpload( event ){
-				this.file = event.target.files[0];
-				this.previewAudio();
-			},
-			
-			previewAudio(){
-				let audio = document.getElementById('audio-preview');
-				let reader = new FileReader();
+  import axios from 'axios'
 
-				reader.readAsDataURL( this.file );
-				reader.addEventListener('load', function(){
-					audio.src = reader.result;
-				});
-			},
+  export default {
+    data() {
+      return {
+        file: '',
+      }
+    },
 
-			submitFile(){
-				let formData = new FormData();
-				
-				formData.append('audio', this.file);
-				
-				axios.post( 'http://127.0.0.1:5000/transcribe',
-					formData,
-					{
-						headers: {
-							'Content-Type': 'multipart/form-data'
-						}
-					}
-				).then(function(){
-					console.log('SUCCESS!!');
-				})
-				.catch(function(){
-					console.log('FAILURE!!');
-				});
-			}
-		}
-	}
+    methods: {
+      handleFileUpload(event) {
+        this.file = event.target.files[0]
+        this.previewAudio()
+      },
+
+      previewAudio() {
+        let audio = document.getElementById('audio-preview')
+        let reader = new FileReader()
+
+        reader.readAsDataURL(this.file)
+        reader.addEventListener('load', function () {
+          audio.src = reader.result
+        })
+      },
+
+      submitFile() {
+        let formData = new FormData()
+
+        formData.append('audio', this.file)
+
+        axios
+          .post('http://127.0.0.1:5000/transcribe', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          })
+          .then(function () {
+            console.log('SUCCESS!!')
+          })
+          .catch(function () {
+            console.log('FAILURE!!')
+          })
+      },
+    },
+  }
 </script>
-	
